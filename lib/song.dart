658d21ea86cad6ed12e4f94f94338c91134a9460
StarfishPrime7 <77465040+StarfishPrime7@users.noreply.github.com>
2024-02-17 14:12:15 -0600

    auto generating apikey

--- lib/song.dart
+++ lib/song.dart
@@ -23,16 +23,49 @@ void main() async {
   var song1 = new Song("title", "artist", "genre");
   print("title: ${song1.title}");
 
-  final String url = 'https://api.spotify.com/v1/recommendations/available-genre-seeds';
-  final String token = 'BQBJCy9PwBqMuO-zklosxOq-M3DcKwW4mDVQ-FT9jG5RbGTRlMAQhDwCpgPb7J-GsM_ZnXnZD9fIZzNDbVgEDCu3rQ_GOacl4S3nsYASY7lRKGx4Jic';
-  print("is this thing working?");
-  final response = await http.get(
-    Uri.parse(url),
-    headers: {'Authorization': 'Bearer $token'},
-  );
-  if (response.statusCode == 200) {
-    print('Response body: ${response.body}');
-  } else {
-    print('Failed to fetch data: ${response.statusCode}');
+   final String refreshToken = 'AQCml-ILsuS9NXX0oa0u03tVOAP7Jwx5dxDoZKhJEX6aLSoF6NkHzBcvjhtDhmiQ5MUw7iMppEH1eNdAum6ugjW89sz7k0Zdl66Q7s_LkB91fIc5q7P_77AVFqRAiGgw0eU'; // Replace with your refresh token
+  try {
+    final String accessToken = await getAccessToken(refreshToken);
+    final String url = 'https://api.spotify.com/v1/recommendations/available-genre-seeds';
+    print("is this thing working?");
+    final response = await http.get(
+      Uri.parse(url),
+      headers: {'Authorization': 'Bearer $accessToken'},
+    );
+    if (response.statusCode == 200) {
+      print('Response body: ${response.body}');
+    } else {
+      print('Failed to fetch data: ${response.statusCode}');
+    }
+  } catch (e) {
+    print('Error: $e');
+  }
+}
+
+Future<String> getAccessToken(String refreshToken) async {
+  final String clientId = 'da3531944d1f4a7fa2c20b63a46d1d60';
+  final String clientSecret = '01c615f0104e4ce585ed871ae37f4490';
+  final String tokenUrl = 'https://accounts.spotify.com/api/token';
+  final String basicAuth =
+      'Basic ${base64Encode(utf8.encode('$clientId:$clientSecret'))}';
+  final Map<String, String> body = {
+    'grant_type': 'refresh_token',
+    'refresh_token': refreshToken,
+  };
+  try {
+    final response = await http.post(
+      Uri.parse(tokenUrl),
+      headers: {'Authorization': basicAuth},
+      body: body,
+    );
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> responseData = json.decode(response.body);
+      final String accessToken = responseData['access_token'];
+      return accessToken;
+    } else {
+      throw Exception('Failed to refresh token: ${response.statusCode}');
+    }
+  } catch (e) {
+    throw Exception('Failed to refresh token: $e');
   }
 }

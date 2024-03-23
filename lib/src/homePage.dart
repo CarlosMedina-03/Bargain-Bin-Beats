527a0ead112b9b605641faf8a53d7d4dadcecc25
Npranata <nathannelson348@gmail.com>
2024-03-23 14:15:34 -0500

    Improve readability for home page

--- lib/src/homePage.dart
+++ lib/src/homePage.dart
@@ -2,62 +2,78 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/src/ColorOptions.dart';
 import 'package:flutter_application_1/src/PlaylistPage.dart';
 import 'package:flutter_application_1/src/tinderPage.dart';
-//import 'package:flutter_application_1/src/genreSelectionPage.dart';
 import 'package:flutter_application_1/src/GenreSelectionPage.dart';
-
+import 'package:swipeable_page_route/swipeable_page_route.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-import 'package:swipeable_page_route/swipeable_page_route.dart';
-
 class HomePage extends StatelessWidget {
   bool startIsPressed = false;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: MEDIUM_PURPLE,
         body: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-                Text(
-                  '''Welcome to\nSong Tinder\n''',
-                  style: TextStyle(fontSize: 26, color: WHITE),
-                ),
-                TextButton( 
-                  style: ButtonStyle(
-                    foregroundColor: MaterialStateProperty.all<Color>(WHITE),
-                    backgroundColor: MaterialStateProperty.all<Color>(DARK_PURPLE),
-                  ),
-                  onPressed: () { 
-                    Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => genreSelectionPage()),
-                    );
-                  },
-                  child: Text("Start", style: TextStyle(fontSize: 18)),
-                  
-                ), 
-                SizedBox(height: 30),
-                TextButton( 
-                  style: ButtonStyle(
-                    foregroundColor: MaterialStateProperty.all<Color>(WHITE),
-                    backgroundColor: MaterialStateProperty.all<Color>(DARK_PURPLE),
+          child: buildContent(context),
+        ),
+      ),
+    );
+  }
 
-                  ),
-                  onPressed: () { 
-                    Navigator.of(context).push(
-                    SwipeablePageRoute(builder: (context) => PlaylistPage(pickedSongs: [],)),
-                    );
-                    
-                  },
-                  child: Text("Playlists", style: TextStyle(fontSize: 18)),
-                ), 
-                SizedBox(height: 30),
-            ]
-          )
-        )
-      )
+  Widget buildContent(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        buildWelcomeText(),
+        SizedBox(height: 30),
+        buildStartButton(context),
+        SizedBox(height: 30),
+        buildPlaylistsButton(context),
+        SizedBox(height: 30),
+      ],
+    );
+  }
+
+  Widget buildWelcomeText() {
+    return const Text(
+      'Welcome to\nSong Tinder\n',
+      style: TextStyle(fontSize: 26, color: WHITE),
+      textAlign: TextAlign.center,
+    );
+  }
+
+  Widget buildStartButton(BuildContext context) {
+    return buildButton(
+      onPressed: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) => const genreSelectionPage()),
+        );
+      },
+      label: "Start",
+    );
+  }
+
+  Widget buildPlaylistsButton(BuildContext context) {
+    return buildButton(
+      onPressed: () {
+        Navigator.of(context).push(
+          SwipeablePageRoute(builder: (context) => PlaylistPage(pickedSongs: [])),
+        );
+      },
+      label: "Playlists",
+    );
+  }
+
+  Widget buildButton({required VoidCallback onPressed, required String label}) {
+    return TextButton(
+      style: ButtonStyle(
+        foregroundColor: MaterialStateProperty.all<Color>(WHITE),
+        backgroundColor: MaterialStateProperty.all<Color>(DARK_PURPLE),
+      ),
+      onPressed: onPressed,
+      child: Text(label, style: TextStyle(fontSize: 18)),
     );
   }
 }
